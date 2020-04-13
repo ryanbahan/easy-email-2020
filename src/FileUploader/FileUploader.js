7bc25afee90c217ecca41d8bade42eca86903e61
Ryan Bahan <ryan@ryanbahan.com>
2020-04-12 18:47:19 -0600

    Add text field component

--- src/FileUploader/FileUploader.js
+++ src/FileUploader/FileUploader.js
@@ -3,9 +3,9 @@ import './FileUploader.css';
 
 export default function FileUploader(props) {
   return (
-    <div class="upload-btn-wrapper">
-      <button class="upload-btn">Upload file</button>
-      <input type="file" name="myfile" class="logo-file-uploader"/>
+    <div className="upload-btn-wrapper">
+      <button className="upload-btn">Upload file</button>
+      <input type="file" name="myfile" className="logo-file-uploader"/>
     </div>
   )
 }
--- src/TextField/TextField.js
+++ src/TextField/TextField.js
@@ -1,6 +1,32 @@
 import React from 'react';
 import './TextField.css';
 
-export default function TextField(props) {
-  return "textfield"
+export default class TextField extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      active: false
+    }
+  }
+
+  toggleModal = (e) => {
+    console.log(this.state.active);
+    this.setState({active: !this.state.active})
+  }
+
+  render() {
+    return (
+      <>
+      <button className="text-field-btn" onClick={() => this.toggleModal()}>
+        Edit
+      </button>
+      <div className={this.state.active ? "modal-wrapper" : "hidden"}>
+        <div className="modal">
+          <p className="close-modal" onClick={() => this.toggleModal()}>X</p>
+          <p>Test</p>
+        </div>
+      </div>
+      </>
+    )
+  }
 }
