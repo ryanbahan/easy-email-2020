b0b3e816fcca24cfade985c69d85087173644f7f
Ryan Bahan <ryan@ryanbahan.com>
2020-04-16 16:31:49 -0600

    Complete functionality

--- src/EmailComponents/EmailContent/EmailContent.js
+++ src/EmailComponents/EmailContent/EmailContent.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-function EmailContent({ content }) {
+function EmailContent({ content, active }) {
 
-  return (
+  return active ? (
     <section className="email-content" style={{padding: "0 1rem"}}>
       <p>{content}</p>
     </section>
-  )
+  ) : null
 }
 
 const mapStateToProps = state => ({
   content: state.form.content,
-  buttonColor: state.form.ctaButtonColor,
-  buttonFontColor: state.form.ctaButtonFontColor,
+  active: state.visibility["Main Content"]
 })
 
 export default connect(mapStateToProps)(EmailContent);
--- src/EmailComponents/EmailFooter/EmailFooter.js
+++ src/EmailComponents/EmailFooter/EmailFooter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-function EmailFooter({ address, fontColor, bgColor }) {
+function EmailFooter({ address, fontColor, bgColor, active }) {
 
   const footerStyle = {
     display: "flex",
@@ -13,17 +13,18 @@ function EmailFooter({ address, fontColor, bgColor }) {
     fontWeight: "500",
   }
 
-  return (
+  return active ? (
         <footer style={footerStyle}>
           <p style={{fontSize: "0.75rem"}}>{address}</p>
         </footer>
-  )
+  ) : null
 }
 
 const mapStateToProps = state => ({
   address: state.form.footerAddress,
   bgColor: state.form.footerBGColor,
   fontColor: state.form.footerFontColor,
+  active: state.visibility["Footer"],
 });
 
 export default connect(mapStateToProps)(EmailFooter);
