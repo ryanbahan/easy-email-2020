47af9c9a728431c101462d65641675cbd4de9836
Ryan Bahan <ryan@ryanbahan.com>
2020-04-16 12:33:04 -0600

    Add Chart.js to display tone sentiment analysis

--- src/FormComponents/Analyzer/Analyzer.js
+++ src/FormComponents/Analyzer/Analyzer.js
@@ -1,17 +1,57 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { parseTones } from '../../utils';
+import Chart from 'chart.js';
 
 class Analyzer extends React.Component {
   constructor() {
     super();
+    this.chartRef = React.createRef();
     this.state = {
-      documentTones: ""
+      documentTones: "",
     }
   }
 
   componentDidMount() {
     this.requestTones();
+    const myChartRef = this.chartRef.current.getContext("2d");
+
+    var myChart = new Chart(myChartRef, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
   }
 
   requestTones = async () => {
@@ -33,6 +73,12 @@ class Analyzer extends React.Component {
   render() {
     return (
       <div className="tone-analysis-view">
+        <canvas
+          id="tone-chart"
+          ref={this.chartRef}
+          style={{width: "50rem", height: "30rem", border: "solid 1px black"}}
+        >
+        </canvas>
       </div>
     )
   }
