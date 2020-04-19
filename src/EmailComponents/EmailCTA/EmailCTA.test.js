c366b0839c0d37f1dd1c01ee0dbc9ca4f6623da3
Ryan Bahan <ryan@ryanbahan.com>
2020-04-19 13:53:05 -0600

    Add initial tests for email components

---
+++ src/EmailComponents/EmailCTA/EmailCTA.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import EmailCTA from './EmailCTA';
+import { render } from '@testing-library/react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from '../../reducers';
+import '@testing-library/jest-dom/extend-expect';
+import { DefaultState } from '../../Templates/DefaultState';
+
+describe("EmailCTA", () => {
+  const store = createStore(rootReducer);
+  let utils;
+
+  beforeEach(() => {
+    utils = render(<Provider store={store}><EmailCTA /></Provider>);
+  })
+
+  it("should render the correct content", () => {
+    const { getByText } = utils;
+
+    const cta = getByText(DefaultState.cta);
+
+    expect(cta).toBeInTheDocument();
+  })
+});
