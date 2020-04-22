d94008c9a7cc42a40fa65953a54db2f8cd7b23e4
Ryan Bahan <ryan@ryanbahan.com>
2020-04-22 16:45:56 -0600

    initial commit

---
+++ src/EmailHTML/index.js
@@ -0,0 +1,23 @@
+export const head = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<meta name="format-detection" content="telephone=no">
+<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=no;">
+<meta http-equiv="X-UA-Compatible" content="IE=9; IE=8; IE=7; IE=EDGE" />
+
+    <title>Page title</title>
+
+    <style type="text/css">
+        @media screen and (max-width: 630px) {
+
+        }
+    </style>
+
+
+</head>`;
+
+export const bodyTop = `<body style="padding:0; margin:0">`;
+
+export const bottom = `</body>
+</html>`;
