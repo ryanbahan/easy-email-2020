c168df079f4149a1c75a5a776d8625e567845423
Ryan Bahan <ryan@ryanbahan.com>
2020-04-26 12:49:06 -0600

    Fix broken test

--- src/reducers/formReducer.test.js
+++ src/reducers/formReducer.test.js
@@ -16,9 +16,12 @@ describe("formReducer", () => {
       mainImage: 'https://via.placeholder.com/1200x600',
       mainImageTagline: 'Download our new e-book for free!',
       mainImageButtonCopy: 'Free Download',
+      mainImageTaglineBG: "#ffffff",
       content: `Happy Easter! And happy birthday to us! 2 years ago, we launched the Darkglass Ultra plugin. Since then we've launched a further 7 plugins. We've got plenty more on our roadmap (s o o n), and we can't wait to show you the Quad Cortex later this year! We'd love to throw a big party, but since nobody can go outside, we're just going to discount all our plugins instead! Get 30% off all plugins until April 18th 00:00 EEST.`,
       cta: 'Call Now',
       footerAddress: 'Company Address',
+      footerBGColor: "#ffffff",
+      headerBGColor: "#ffffff",
     };
 
     const result = formReducer(DefaultState, {
