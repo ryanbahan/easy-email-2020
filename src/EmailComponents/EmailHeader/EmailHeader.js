a0884dbd8e240fd2b458cf8c5458b8aa8c49a7ad
Ryan Bahan <ryan@ryanbahan.com>
2020-04-27 15:47:32 -0600

    Add loading state to header and main image

--- src/EmailComponents/EmailHeader/EmailHeader.js
+++ src/EmailComponents/EmailHeader/EmailHeader.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import ImageLoadingSpinner from '../../ImageLoadingSpinner/ImageLoadingSpinner';
 
 const EmailHeader = ({
   bgColor,
   companyImage,
   companyName,
   companyFontColor,
-  active }) => {
+  active,
+  loading
+  }) => {
   return active ? (
     <table border="0" cellSpacing="0" cellPadding="0" width="600px" height="105px" style={{color: companyFontColor, backgroundColor: bgColor}}>
       <tbody>
         <tr>
-          <td width="85px"><img src={companyImage} alt="Company logo" border="0" style={{display: "block", width: "75px", margin: "0"}} /></td>
+          <td width="85px">
+            <div className="company-logo-container" style={{position: "relative", width: "min-content"}}>
+              {loading && <ImageLoadingSpinner />}
+              <img src={companyImage} alt="Company logo" border="0" style={{display: "block", width: "75px", margin: "0"}} />
+            </div>
+          </td>
           <td style={{fontSize: "25px"}}>{companyName}</td>
         </tr>
       </tbody>
@@ -27,6 +35,7 @@ const mapStateToProps = state => ({
   companyFontColor: state.form.companyFontColor,
   bgColor: state.form.headerBGColor,
   active: state.visibility["Header"],
+  loading: state.loading,
 })
 
 EmailHeader.propTypes = {
