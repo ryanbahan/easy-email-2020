b000468afe5418c5165ab069cdd4d933cff0f706
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-29 18:52:20 +0000

    Deploy ryanbahan/easy-email-2020 to github.com/ryanbahan/easy-email-2020.git:gh-pages

---
+++ precache-manifest.81c0bcd3f0a718066ce65872a5a2a1c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01be577512696cd9c809c962e3c8abe7",
+    "url": "/easy-email-2020/index.html"
+  },
+  {
+    "revision": "4ad0da1cc8e412cae4f9",
+    "url": "/easy-email-2020/static/css/2.3c515868.chunk.css"
+  },
+  {
+    "revision": "8b3943e3f1a1a3965643",
+    "url": "/easy-email-2020/static/css/main.026cf9ba.chunk.css"
+  },
+  {
+    "revision": "4ad0da1cc8e412cae4f9",
+    "url": "/easy-email-2020/static/js/2.a9d1af93.chunk.js"
+  },
+  {
+    "revision": "4a882b6a354800c5006aea3fdf5b341f",
+    "url": "/easy-email-2020/static/js/2.a9d1af93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b3943e3f1a1a3965643",
+    "url": "/easy-email-2020/static/js/main.2bd2b568.chunk.js"
+  },
+  {
+    "revision": "7bd16d543489c1875b43",
+    "url": "/easy-email-2020/static/js/runtime-main.fa6e3e9b.js"
+  }
+]);
